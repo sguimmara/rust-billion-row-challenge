ee2baaa60309a06eaf90a48edca2e2a6910afcfe
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2024-01-19 15:34:41 +0100

    setup clap

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// File to read
+    #[arg(short, long)]
+    input: String,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let path = std::path::Path::new(&args.input);
+
+    if !path.exists() {
+        eprintln!("no such file: {}", args.input);
+        std::process::exit(1);
+    }
+}
